503256edf3812ae785a2502be9383464eba33c9e
DESKTOP-OMIIJ1H\Mi PC <miguel.graham@xarxatecactiva.com>
2022-06-16 19:39:00 +0200

    Commit. revisar siempre tengo la misma fecha de la semana con diferentes fechas ingresadas

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let dateAsChain;
+dateAsChain = parseInt(prompt("Ingrese el año 00/00/0000"));
+const days = ['domingo','lunes','martes','miércoles','jueves','viernes','sábado','domingo',];
+const numberDay = new dateAsChain().getDay();
+const nameDay = days[numberDay];
+console.log("Nombre de día de la semana: ", nameDay);
